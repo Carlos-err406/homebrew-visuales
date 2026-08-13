25e001052ce42a3cfad32507a07a618eb019e1d9
Carlos Daniel Vilaseca Illnait <carlosvilaseca406@gmail.com>
2026-08-13 18:14:00 -0400

    Update visuales formula to 1.0.4

--- Formula/visuales.rb
+++ Formula/visuales.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Visuales < Formula
   desc "Search and download visuales.uclv.cu content from the terminal"
   homepage "https://github.com/Carlos-err406/visuales-search"
-  url "https://registry.npmjs.org/visuales/-/visuales-1.0.3.tgz"
-  sha256 "c6d37580eb984d499fd42ec12e46fb659ca11f2c7b5b740428c1046adb3fb4ad"
+  url "https://registry.npmjs.org/visuales/-/visuales-1.0.4.tgz"
+  sha256 "b0e2ce7cde8d852b104c2bec1dc2159b2b3a6a1c69192cfb0ac61ac65ae6c0ed"
   license "MIT"
 
   depends_on "node"
